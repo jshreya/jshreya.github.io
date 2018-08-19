ab02b847e9c3533ee65cdab802632d96338903b4
jshreya <joglekar.shreya@gmail.com>
2018-08-18 20:21:37 -0400

    basic to do list

--- todo.js
+++ todo.js
@@ -6,5 +6,13 @@ const heading = <h1> To Do list for {today.getMonth() + 1}/{today.getDate()}/{to
 ReactDOM.render(heading, document.getElementById('todoHeading'));
 // set to do list
 
-
+class ToDoList extends React.Component {
+  render() {
+    <ul>
+      <li> Grocery shopping </li>
+      <li> Clean room </li>
+    </ul>
+  }
+}
+ReactDOM.render(<ToDoList/>, document.getElementById('todoList'));
 // set up for adding new items
