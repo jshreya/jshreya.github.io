a8581a122374afb303e26476183eff82a3fb65a3
jshreya <joglekar.shreya@gmail.com>
2018-08-19 13:09:24 -0400

    loop through to do list state

--- todo.js
+++ todo.js
@@ -4,18 +4,25 @@
 const today = new Date();
 const heading = <h1> To Do list for {today.getMonth() + 1}/{today.getDate()}/{today.getFullYear()} : </h1>;
 ReactDOM.render(heading, document.getElementById('todoHeading'));
-// set to do list
 
+// set to do list
 class ToDoList extends React.Component {
+  constructor(props) {
+    this.state.todoItems = ["Grocery shopping", "Clean room"];
+    this.state.completedItems = [];
+  }
   render() {
-    const list = (
-      <ul>
-        <li> Grocery shopping </li>
-        <li> Clean room </li>
-      </ul>
-    );
-    return list;
+    const list = getToDoItems();
+    return <ul> {list} </ul>;
+  }
+  getToDoItems() {
+    const todoItems = [];
+    this.state.todoItems.forEach(function(element) {
+      todoItems.push(<li>{element}</li>);
+    });
+    return todoItems;
   }
 }
 ReactDOM.render(<ToDoList/>, document.getElementById('todoList'));
+
 // set up for adding new items
