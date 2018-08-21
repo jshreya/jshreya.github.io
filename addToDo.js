55d1826bb4e9ecf9f790fec82d55cfd823ef4284
jshreya <joglekar.shreya@gmail.com>
2018-08-20 21:30:59 -0400

    styling and reference error

--- addToDo.js
+++ addToDo.js
@@ -1,18 +1,22 @@
 'use strict';
 
 // set up for adding new items
-export default class NewToDo extends React.Component {
+class NewToDo extends React.Component {
   render() {
     const addToDo = (
-      <div>
-        <input type="text" id="newToDoItem" ref="newToDoItem"/>
-        <input type="button" id="addToDoItem" onClick={this.addNewToDoItem.bind(this)}/>
+      <div id="newToDo">
+        <input type="text" id="newToDoItem" ref="newToDoItem" required/>
+        <span className="button" id="add" onClick={this.addNewToDoItem.bind(this)}> <span className="glyphicon glyphicon-plus"></span> </span>
       </div>
     );
     return addToDo;
   }
-  addNewToDoItem() {
-    alert("Add " + this.refs.newToDoItem.value);
+  addNewToDoItem(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    //alert("Add " + this.refs.newToDoItem.value);
     this.props.addToDo(this.refs.newToDoItem.value);
+    this.refs.newToDoItem.value="";
   }
 }
+export default NewToDo;
--- toDoList.js
+++ toDoList.js
@@ -1,28 +1,48 @@
 'use strict';
 
-import NewToDo from 'addToDo.js';
+import NewToDo from './addToDo.js';
+
+// class NewToDo extends React.Component {
+//   render() {
+//     const addToDo = (
+//       <div id="newToDo">
+//         <input type="text" id="newToDoItem" ref="newToDoItem" required/>
+//         <span className="button" id="add" onClick={this.addNewToDoItem.bind(this)}> <span className="glyphicon glyphicon-plus"></span> </span>
+//       </div>
+//     );
+//     return addToDo;
+//   }
+//   addNewToDoItem(e) {
+//     e.preventDefault();
+//     e.stopPropagation();
+//     //alert("Add " + this.refs.newToDoItem.value);
+//     this.props.addToDo(this.refs.newToDoItem.value);
+//     this.refs.newToDoItem.value="";
+//   }
+// }
 
 // set to do list heading
 const today = new Date();
-const heading = <h1> To Do list for {today.getMonth() + 1}/{today.getDate()}/{today.getFullYear()} : </h1>;
+const heading = <h1> To-do list for {today.getMonth() + 1}/{today.getDate()}/{today.getFullYear()} : </h1>;
 ReactDOM.render(heading, document.getElementById('todoHeading'));
 
 // set to do list
 class ToDoList extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
         todoItems: ["Grocery shopping", "Clean room"],
         completedItems: []
     };
+    this.addItem = this.addItem.bind(this);
   }
   render() {
     //const list = this.getToDoItems();
     const list = this.state.todoItems.map((element, index) =>
       <li key={index}>
-        {element}
-        <input type="button" onClick={this.itemCompleted.bind(this, index)} value="Done!"/>
-        <input type="button" onClick={this.deleteItem.bind(this, index)} value="Delete"/>
+        <span id='todo'>{element}</span>
+        <span className="button" id="done" onClick={this.itemCompleted.bind(this, index)}> <span className="glyphicon glyphicon-ok"></span> </span>
+        <span className="button" id="delete" onClick={this.deleteItem.bind(this, index)}> <span className="glyphicon glyphicon-remove"></span> </span>
       </li>
     );
     return (
@@ -36,22 +56,22 @@ class ToDoList extends React.Component {
   }
 
   itemCompleted(key) {
-      alert("completed " + this.state.todoItems[key]);
+      //alert("completed " + this.state.todoItems[key]);
       this.setState((prevState, props) => {
         const updatedList = prevState.todoItems.filter((element, index) => index !== key);
         return {todoItems: updatedList};
       });
   }
   deleteItem(key) {
-    alert("delete " + this.state.todoItems[key]);
+    //alert("delete " + this.state.todoItems[key]);
     this.setState((prevState, props) => {
       const updatedList = prevState.todoItems.filter((element, index) => index !== key);
       return {todoItems: updatedList};
     });
   }
   addItem(item) {
-    alert("Adding " + item);
-    this.setState((prevState, props) => {
+    //alert("Adding " + item);
+    this.setState((prevState) => {
       prevState.todoItems.push(item);
       return {todoItems: prevState.todoItems};
     });
